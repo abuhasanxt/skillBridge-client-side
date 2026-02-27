3f3d2a3ed77b41e527a67465ed8b1bffbc31bc89
unknown <abuhasanxt@gmail.com>
2026-02-27 17:18:04 +0600

    added env file type

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,13 @@
 
+import { env } from "@/env";
 import { cookies } from "next/headers";
-const AUTH_API=process.env.AUTH_URL
+const AUTH_URL=env.AUTH_URL
 export const userService = {
   getSession: async function () {
     try {
       const cookieStore = await cookies();
 
-      const res = await fetch(`${AUTH_API}/get-session`, {
+      const res = await fetch(`${AUTH_URL}/get-session`, {
         headers: {
           cookie: cookieStore.toString(),
         },
